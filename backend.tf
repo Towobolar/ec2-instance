dce61cf9801d4ba0ec267bc64ca6f21ee0570b64
Towobolar <71108083+Towobolar@users.noreply.github.com>
2024-06-24 17:03:24 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@
     terraform {
       backend "remote" {
         # The name of your Terraform Cloud organization.
-        organization = "abbey-terraform-aws"
+        organization = "Cloud-Platform"
 
         # The name of the Terraform Cloud workspace to store Terraform state files in.
         workspaces {
